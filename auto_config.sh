cb233f659686c069b8ee1dd18b7424c464e738d9
novec.tech <90937877+novec-tech@users.noreply.github.com>
2022-04-15 10:30:57 +0200

    Update auto_config.sh

--- auto_config.sh
+++ auto_config.sh
@@ -1,3 +1,10 @@
-!# /bin/sh
+#!/bin/bash
+# update & upgrade
 sudo apt install update -y
-sudo apt install upgrade -y 
+sudo apt install upgrade -y
+
+# official Latest Docker Install
+curl -fsSL https://get.docker.com -o get-docker.sh
+# run the file
+sudo sh get-docker.sh
+
